efbf79a1c6e74a02d069aec2aab34b0934669142
Povilas Lajus <povilas.lajus@gmail.com>
2022-03-14 22:23:30 +0200

    feat(registration): add full name

--- forms.py
+++ forms.py
@@ -1,12 +1,15 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField, BooleanField
-from wtforms.validators import DataRequired, Email, EqualTo
+from wtforms.validators import DataRequired, Email, EqualTo, InputRequired, Length
 
 class RegistrationForm(FlaskForm):
 
     """User registration form"""
 
     email = StringField('Email', validators=[DataRequired(), Email()])
+
+    first_name = StringField('First name', validators=[InputRequired(), Length(min=1, max=50)])
+    last_name = StringField('Last name', validators=[InputRequired(), Length(min=1, max=50)])
 
     password = PasswordField('Password', validators=[DataRequired()])
     confirm_password = PasswordField('Confirm password',
